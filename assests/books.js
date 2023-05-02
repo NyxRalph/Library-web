a9dff1bf0770bef14004671cf98fa2cb5bed5590
Ralph Andy Menz <andyralphmenz@gmail.com>
2023-05-02 23:26:18 +0000

    improvements

---
+++ assests/books.js
@@ -0,0 +1,85 @@
+
+function getBooks(){
+    return[
+        {
+            id: 1,
+            title:  "The 50th Law",
+            url: "assests/img/bk1.jpg",
+            originalPrice: 49.95,
+            salesPrice: 71,
+            rating: 4
+        },
+        {
+            id: 2,
+            title:  "The Laws of Human Nature",
+            url: "assests/img/bk2.jpg",
+            originalPrice: 49.95,
+            salesPrice: 110,
+            rating: 2.5
+        },
+        {
+            id: 3,
+            title:  "Daily Laws",
+            url: "assests/img/bk3.jpg",
+            originalPrice: 49.95,
+            salesPrice: 14.95,
+            rating: 3.5
+        },
+        {
+            id: 4,
+            title:  "The Art of Seduction",
+            url: "assests/img/bk4.jpg",
+            originalPrice: 49.95,
+            salesPrice: 32.95,
+            rating: 5 
+        },
+        {
+            id: 5,
+            title:  "The Work book",
+            url: "assests/img/bk5.jpg",
+            originalPrice: 49.95,
+            salesPrice: 24.95,
+            rating: 4
+        },
+        {
+            id: 6,
+            title:  "The 33 Trategies",
+            url: "assests/img/bk6.jpg",
+            originalPrice: 49.95,
+            salesPrice: 14,
+            rating: 4.5
+        },
+        {
+            id: 7,
+            title:  "The 50th Law",
+            url: "assests/img/bk1.jpg",
+            originalPrice: 49.95,
+            salesPrice: 17.95,
+            rating: 4
+        },
+        {
+            id: 8,
+            title:  "The Laws of Human Nature",
+            url: "assests/img/bk2.jpg",
+            originalPrice: 49.95,
+            salesPrice: 12.65,
+            rating: 3.5
+        },
+        {
+            id: 9,
+            title:  "Daily Laws",
+            url: "assests/img/bk3.jpg",
+            originalPrice: 49.95,
+            salesPrice: 7,
+            rating: 4
+        },
+        {
+            id: 10,
+            title:  "The Art of Seduction",
+            url: "assests/img/bk4.jpg",
+            originalPrice: 49.95,
+            salesPrice: 14,
+            rating: 5
+        }
+    ]
+}
